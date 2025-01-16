3ac32e1bc027c836e11e4ec955d50c766cdc8128
DDale21 <christiandalepancho@gmail.com>
2025-01-17 08:04:09 +1300

    App: Refactored Loading and Saving of todos from Local Storage. Added validation and Error handling.

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,37 @@ import Todos from './components/Todos';
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+  const [todos, setTodos] = useState(loadTodosFromLocalStorage());
 
   useEffect(() => {
     console.log(todos);
-    if (todos) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
+    saveTodosToLocalStorage(todos)
   }, [todos]);
+
+  function loadTodosFromLocalStorage() {
+    try {
+      const localTodos = localStorage.getItem('todos');
+      if (!localTodos) {
+        return [];
+      } 
+      return JSON.parse(localTodos);
+    } catch (err) {
+      console.error("Failed to load todos", err)
+      return [];
+    }
+  }
+
+  function saveTodosToLocalStorage(todos) {
+    try {
+      if (!todos) {
+        throw new Error("Cannot save empty todos");
+      }
+      const localTodos = JSON.stringify(todos);
+      localStorage.setItem('todos', localTodos)
+    } catch (err) {
+      console.error("Failed to save todos", err)
+    }
+  }
 
   function removeTodoById(todoId) {
     const newTodo = todos.filter(({id}) => id !== todoId);
